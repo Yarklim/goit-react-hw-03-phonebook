b30e17b55df107a6d8ad53c07e49b1978ebc9d55
Yaroslav Klimas <yarklim@gmail.com>
2023-03-29 21:18:54 +0300

    added task 1 solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,31 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm';
 import { Filter } from './Filter';
 import { ContactList } from './ContactList';
+import { EmergencyBtn } from './EmergencyBtn';
+import { Modal } from './Modal';
+import { EmergencyContent } from './EmergencyContent';
 import { Container } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+    showModal: false,
   };
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
 
   handleSubmit = e => {
     e.preventDefault();
@@ -61,6 +74,12 @@ export class App extends Component {
     return filterContactsList;
   };
 
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
   render() {
     return (
       <Container>
@@ -76,6 +95,12 @@ export class App extends Component {
           contacts={this.handleFiltered()}
           onDelete={this.handleDelete}
         />
+        <EmergencyBtn onClick={this.toggleModal} title="EMERGENCY NUMBERS!!!" />
+        {this.state.showModal && (
+          <Modal onClose={this.toggleModal}>
+            {<EmergencyContent onClose={this.toggleModal} />}
+          </Modal>
+        )}
       </Container>
     );
   }
